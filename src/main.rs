6df968e8231c07cf2f29a319d1b127e91e0f459b
adii <77727680+frozen-beak@users.noreply.github.com>
2024-10-26 19:05:30 +0530

    unfinished

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero2prod::startup::run;
 
 #[tokio::main]
 
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8080")
+        .expect("Unable to bind to 8080 port");
+
+    run(listener)?.await
 }
