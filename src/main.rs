1ddada186948ac82d64e429444eb1b72df4207ab
adii <77727680+frozen-beak@users.noreply.github.com>
2024-12-17 19:51:26 +0530

    scripts, migrations & restructure
    - Added script for db creation
    - DB migrations
    - Folder Restructure
    - Decoupling of routes

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use std::net::TcpListener;
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
 
-    zero2prod::run(listener)?.await
+    zero2prod::startup::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     // We return the application address to the caller!
     format!("http://127.0.0.1:{}", port)
@@ -34,6 +34,7 @@ async fn subscribe_returns_200_for_a_valid_form_data() {
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
         .body(body)
+        .header("Content-Type", "application/x-www-form-urlencoded")
         .send()
         .await
         .expect("Failed to execute request");
