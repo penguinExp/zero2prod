58cba03e4a0e6630accf02c2152140c720a50f18
adii <77727680+frozen-beak@users.noreply.github.com>
2024-12-18 16:40:41 +0530

    added telemetry logs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::net::TcpListener;
-
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
-use zero2prod::config::get_configuration;
+use std::net::TcpListener;
+use zero2prod::{
+    config::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // panic if we can't read config's
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+
+    init_subscriber(subscriber);
+
     let config = get_configuration().expect("Failed to read config");
-    let connection_pool = PgPool::connect(&config.database.connection_string())
+
+    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres");
 
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
 
-    zero2prod::startup::run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,19 @@
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(db_pool);
+    let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
